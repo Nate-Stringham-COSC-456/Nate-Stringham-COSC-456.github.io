5f0f6c9bede8c56235f76822f8f4cfaf7c37a78d
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-26 11:49:17 -0400

    feat: ✨ add Vec4

--- src/vector.ts
+++ src/vector.ts
@@ -1,5 +1,5 @@
 export class Vec2 extends Float32Array {
-  constructor(...args: [] | [number, number] | [Vec2]) {
+  constructor(...args: [] | [Vec2] | [number, number]) {
     if (args.length === 0) {
       super(2);
     } else if (args.length === 1) {
@@ -13,12 +13,26 @@ export class Vec2 extends Float32Array {
 }
 
 export class Vec3 extends Float32Array {
-  constructor(...args: [] | [number, number, number] | [Vec3]) {
+  constructor(...args: [] | [Vec3] | [number, number, number]) {
     if (args.length === 0) {
       super(3);
     } else if (args.length === 1) {
       super(args[0]);
     } else if (args.length === 3) {
+      super(args);
+    } else {
+      throw new Error("Invalid number of arguments");
+    }
+  }
+}
+
+export class Vec4 extends Float32Array {
+  constructor(...args: [] | [Vec4] | [number, number, number, number]) {
+    if (args.length === 0) {
+      super(4);
+    } else if (args.length === 1) {
+      super(args[0]);
+    } else if (args.length === 4) {
       super(args);
     } else {
       throw new Error("Invalid number of arguments");
@@ -33,7 +47,7 @@ export class Vec3 extends Float32Array {
  * @param s value ued to interpolate between u and v
  * @returns the interpolated value
  */
-export function mix<T extends number | Vec2 | Vec3>(u: T, v: T, s: number): T {
+export function mix<T extends number | Vec2 | Vec3 | Vec4>(u: T, v: T, s: number): T {
   const s_ = 1 - s;
   if (typeof u == "number" && typeof v == "number") {
     return (s_ * u + s * v) as T;
@@ -41,6 +55,8 @@ export function mix<T extends number | Vec2 | Vec3>(u: T, v: T, s: number): T {
     return new Vec2(s_ * u[0] + s * v[0], s_ * u[1] + s * v[1]) as T;
   } else if (u instanceof Vec3 && v instanceof Vec3) {
     return new Vec3(s_ * u[0] + s * v[0], s_ * u[1] + s * v[1], s_ * u[2] + s * v[2]) as T;
+  } else if (u instanceof Vec4 && v instanceof Vec4) {
+    return new Vec4(s_ * u[0] + s * v[0], s_ * u[1] + s * v[1], s_ * u[2] + s * v[2], s_ * u[3] + s * v[3]) as T;
   } else {
     throw new Error("Invalid arguments");
   }
